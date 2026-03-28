4a3de056af4c05ac3dd0a32f5f17a1b16be274bb
pacocayetano <pacocayetano29@gmail.com>
2026-03-28 18:43:19 +0100

    Parte 2.3 - Add transform.sql for idempotent data transformation
    Add SQL transformation to move data from SANDBOX to INTEGRATION layer.
    
    The query removes duplicates using ROW_NUMBER() and ensures idempotency using a MERGE statement.

--- sql/transform.sql
+++ sql/transform.sql
@@ -1,1 +1,21 @@
-
+MERGE `data-engineer-test-491612.INTEGRATION.integration_prueba_tecnica` AS target
+USING (
+  SELECT
+    id,
+    postId,
+    name,
+    email,
+    body,
+    CURRENT_DATE() AS load_date
+  FROM (
+    SELECT
+      *,
+      ROW_NUMBER() OVER (PARTITION BY id ORDER BY id) AS row_num
+    FROM `data-engineer-test-491612.SANDBOX_api_test.api_comments`
+  )
+  WHERE row_num = 1
+) AS source
+ON target.id = source.id
+WHEN NOT MATCHED THEN
+  INSERT (id, postId, name, email, body, load_date)
+  VALUES (source.id, source.postId, source.name, source.email, source.body, source.load_date);
